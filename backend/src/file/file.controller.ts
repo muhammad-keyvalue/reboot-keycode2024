41add27bb287d77bbbd886b633a29486166bab6c
Muhammad <muhammad.cv@keyvalue.systems>
2024-09-27 15:17:43 +0530

    feat: Add API to download image

--- backend/src/file/file.controller.ts
+++ backend/src/file/file.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
+import { FileService } from './file.service';
+import { Response } from 'express';
 
 @Controller('file')
-export class FileController {}
+export class FileController {
+    constructor(private _fileService: FileService) {}
+    @Get('/:imageName')
+    async findAll(@Param('imageName') imageName: string, @Res() res: Response) {
+      return this._fileService.getImage(imageName,res);
+    }
+
+}
--- backend/src/file/file.service.ts
+++ backend/src/file/file.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { createReadStream, existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+    getImage(imageName: string, res: Response) {
+        const filePath = join(process.cwd(), '..', '/backend/src/processed', imageName);
+        // Check if the file exists
+        if (existsSync(filePath)) {
+          const stream = createReadStream(filePath);
+          stream.pipe(res);
+        } else {
+            throw new NotFoundException('Image not found');
+        }
+      }
+}
