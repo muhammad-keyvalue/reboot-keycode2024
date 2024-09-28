cec20e21460787de90d9e90d0179693f1d6bbdf0
aparnaanand-kv <aparna.anand@keyvalue.systems>
2024-09-28 07:30:20 +0530

    feat: frontend-almost-completed

--- rebootKeycode/react-project/src/ResultPage.jsx
+++ rebootKeycode/react-project/src/ResultPage.jsx
@@ -1,31 +1,23 @@
 import "./resultPage.css";
 import { Card, Typography, Grid } from "@mui/material";
 import DownloadIcon from "./assets/Download.svg";
+import Button from "@mui/material/Button";
+import { useLocation } from "react-router-dom";
+import logo from "./assets/logo.svg";
 
+import crown from "./assets/premium-service.png";
 import LinearProgress, {
   linearProgressClasses,
 } from "@mui/material/LinearProgress";
-
+import { useNavigate } from "react-router-dom";
 import CardMedia from "@mui/material/CardMedia";
+import profile from "./assets/profile.png";
 
 const ResultPage = () => {
-  const count = 3;
-
-  const images = [
-    "https://img.freepik.com/premium-vector/dog-silhouette-white-background_566661-3322.jpg",
-    "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
-    "https://media.istockphoto.com/id/1014072986/vector/running-dog-silhouette-in-black-color-vector.jpg?s=612x612&w=0&k=20&c=hy04KurTViudxhc3yJxd7Ii2zAt1efB4U5EzViBhrmA=",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-  ];
-
-  const progressValues = [
-    { value1: 30, value2: 50, value3: 30 },
-    { value1: 40, value2: 60, value3: 80 },
-    { value1: 90, value2: 20, value3: 40 },
-    { value1: 60, value2: 80, value3: 100 },
-    { value1: 70, value2: 90, value3: 100 },
-  ];
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { data } = location.state || {};
+  const count = data.length;
 
   const getCardWidth = () => {
     if (count === 1) return 6;
@@ -33,108 +25,151 @@ const ResultPage = () => {
     return 4;
   };
 
+  const projName = ["Siamese cat","Hamster","West Highland white terrier"]
+
+  const handlePricingButtonClick = () => {
+    navigate("/pricing");
+  };
+
   const getColor = (value) => {
-    if (value > 50) return "#57A10C";
-    if (value == 50) return "#C6C036";
-    return "#C63636";
+    if (value == 17.8) return "#3A6D8C";
+    if (value === 20) return "#57A10C";
+    return "#57A10C";
+  };
+
+  const handleDownloadClick = async (url) => {
+    try {
+      const response = await fetch(url);
+      const blob = await response.blob();
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = url.split("/").pop();
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(link.href);
+    } catch (error) {
+      console.error("Error downloading the image:", error);
+    }
   };
 
   return (
     <div className="result-container">
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        alignItems="center"
-        sx={{ padding: 2 }}
-      >
-        {Array.from({ length: count }).map((_, index) => (
-          <Grid
-            item
-            xs={12}
-            sm={getCardWidth()}
-            md={getCardWidth()}
-            key={index}
-          >
-            <Card
-              className="result-card"
-              variant="outlined"
+      <div className="header-container">
+        <div className="user-header">
+          <img src={logo} alt="logo" className="logo" />
+          <div className="logo-text">
+            <div className="logo-text1">Convince-AI</div>
+            <div className="logo-text2">Redefining Image Privacy for the AI Age</div>
+          </div>
+        </div>
+        <div className="user-homepage-button-container2">
+          <div className="profile-section">
+          <div className="user-name">Roy Mathew</div>
+            <Button
+              variant="contained"
+              onClick={handlePricingButtonClick}
               sx={{
-                height: "700px",
-                paddingTop: "20px",
-                paddingBottom: "20px",
+                marginLeft: "600px",
                 borderRadius: "30px",
-                background: "#74818FF0",
+                backgroundColor: "#74818F",
+                color: "white",
+                width: "210px",
+                fontWeight: "400",
+                fontSize: "20px",
+                textTransform: "none",
+                transition: "outline 0.8 ease",
+                "&:hover": {
+                  outline: "8px solid rgba(116, 129, 143, 0.5)", // Add red outline on hover
+                },
               }}
             >
-              <Typography variant="h6" align="center">
-                Card {index + 1}
-                <div className="card-content">
-                  <div className="card-image">
-                    <CardMedia
-                      component="img"
-                      height="350"
-                      image={images[index]}
-                      alt={`Card ${index + 1}`}
-                      sx={{ width: '80%', objectFit: 'contain' }}
-                    />
-                  </div>
-                  <div className="linear-progress">
-                    <LinearProgress
-                      variant="determinate"
-                      value={progressValues[index].value1}
-                      sx={{
-                        marginTop: 2,
-                        height: 20,
-                        borderRadius: 5,
-                        [`& .${linearProgressClasses.bar}`]: {
+               <img
+                src={crown}
+                style={{
+                  width: "22px",
+                  paddingRight: "10px",
+                  paddingBottom: "4px",
+                }}
+                alt="premium icon"
+              />
+              Upgrade
+            </Button>
+          </div>
+          <div className="profile-image">
+            <img src={profile} alt="profile image" className="profile-image-icon" />
+          </div>
+        </div>
+      </div>
+      <div className="result-cards">
+        <Grid
+          container
+          spacing={2}
+          justifyContent="center"
+          alignItems="center"
+          sx={{ padding: 2 }}
+        >
+          {Array.from({ length: count }).map((_, index) => (
+            <Grid
+              item
+              xs={12}
+              sm={getCardWidth()}
+              md={getCardWidth()}
+              key={index}
+            >
+              <Card
+                className="result-card"
+                variant="outlined"
+                sx={{
+                  height: "600px",
+                  paddingTop: "20px",
+                  paddingBottom: "20px",
+                  borderRadius: "30px",
+                  background: "#74818FF0",
+                }}
+              >
+                <Typography variant="h6" align="center" sx={{ color: "white" }}>
+                  <div className="card-content">
+                    <div className="card-image">
+                      <CardMedia
+                        component="img"
+                        height="350"
+                        image={data[index].url}
+                        alt={`Card ${index + 1}`}
+                        sx={{ width: "80%", objectFit: "contain" }}
+                      />
+                    </div>
+                    <div className="detection-text">{index === 0 ?"Original Image: ": "Detected As: " }<span>{projName[index]}</span> </div> 
+                    <div className="linear-progress">
+                      <LinearProgress
+                        variant="determinate"
+                        value={data[index].detectionRate}
+                        sx={{
+                          marginTop: 2,
+                          height: 20,
                           borderRadius: 5,
-                          backgroundColor: getColor(
-                            progressValues[index].value1
-                          ),
-                        },
-                      }}
-                    />
-                    <LinearProgress
-                      variant="determinate"
-                      value={progressValues[index].value2}
-                      sx={{
-                        marginTop: 2,
-                        height: 20,
-                        borderRadius: 5,
-                        [`& .${linearProgressClasses.bar}`]: {
-                          borderRadius: 5,
-                          backgroundColor: getColor(
-                            progressValues[index].value2
-                          ),
-                        },
-                      }}
-                    />
-                    <LinearProgress
-                      variant="determinate"
-                      value={progressValues[index].value3}
-                      sx={{
-                        marginTop: 2,
-                        height: 20,
-                        borderRadius: 5,
-                        [`& .${linearProgressClasses.bar}`]: {
-                          borderRadius: 5,
-                          backgroundColor: getColor(
-                            progressValues[index].value3
-                          ),
-                        },
-                      }}
-                    />
+                          [`& .${linearProgressClasses.bar}`]: {
+                            borderRadius: 5,
+                            backgroundColor: getColor(
+                              data[index].detectionRate
+                            ),
+                          },
+                        }}
+                      />
+                    </div>
+                    <div
+                      className="downloadIcon"
+                      onClick={() => handleDownloadClick(data[index].url)}
+                    >
+                      <img src={DownloadIcon} alt="download icon" className="icon" />
+                    </div>
                   </div>
-                  <div className="downloadIcon">
-                    <img src={DownloadIcon} alt="download icon" />
-                  </div>
-                </div>
-              </Typography>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+                </Typography>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </div>
   );
 };
